b5ae6e5c21b47e930e731b11cebc763f0d8a9b4d
Serhii Horb <43185034+Serhii-Horb@users.noreply.github.com>
2024-02-10 12:40:19 +0100

    Update NumericalOperations.java

--- src/main/java/org/example/Ex1/NumericalOperations.java
+++ src/main/java/org/example/Ex1/NumericalOperations.java
@@ -1,1 +1,11 @@
-package org.example.Ex1;public interface NumericalOperations<T extends Number> {    T add(T a, T b);    T sub(T a, T b);    T multi(T a, T b);    T div(T a, T b);}
+package org.example.Ex1;
+
+public interface NumericalOperations<T extends Number> {
+    T add(T a, T b);
+
+    T sub(T a, T b);
+
+    T multi(T a, T b);
+
+    T div(T a, T b);
+}
