6297031fc4080ff7dde2f35efd0894a9135de571
Serhii Horb <43185034+Serhii-Horb@users.noreply.github.com>
2024-02-10 12:39:27 +0100

    Update IntegerProcessing.java

--- src/main/java/org/example/Ex1/IntegerProcessing.java
+++ src/main/java/org/example/Ex1/IntegerProcessing.java
@@ -1,1 +1,27 @@
-package org.example.Ex1;public class IntegerProcessing implements NumericalOperations<Integer> {    @Override    public Integer add(Integer a, Integer b) {        return a + b;    }    @Override    public Integer sub(Integer a, Integer b) {        return a - b;    }    @Override    public Integer multi(Integer a, Integer b) {        return a * b;    }    @Override    public Integer div(Integer a, Integer b) {        if (b != 0) {            return a / b;        } else {            throw new ArithmeticException("На 0 делить нельзя");        }    }}
+package org.example.Ex1;
+
+public class IntegerProcessing implements NumericalOperations<Integer> {
+    @Override
+    public Integer add(Integer a, Integer b) {
+        return a + b;
+    }
+
+    @Override
+    public Integer sub(Integer a, Integer b) {
+        return a - b;
+    }
+
+    @Override
+    public Integer multi(Integer a, Integer b) {
+        return a * b;
+    }
+
+    @Override
+    public Integer div(Integer a, Integer b) {
+        if (b != 0) {
+            return a / b;
+        } else {
+            throw new ArithmeticException("На 0 делить нельзя");
+        }
+    }
+}
