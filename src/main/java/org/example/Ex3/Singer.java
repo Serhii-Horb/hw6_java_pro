2acf89dea47a476e778dd66c8e902f13bbdda428
Serhii Horb <43185034+Serhii-Horb@users.noreply.github.com>
2024-02-10 12:42:12 +0100

    Update Singer.java

--- src/main/java/org/example/Ex3/Singer.java
+++ src/main/java/org/example/Ex3/Singer.java
@@ -1,1 +1,7 @@
-package org.example.Ex3;public class Singer extends Artist {    public Singer(String name, int age, String skills) {        super(name, age, skills);    }}
+package org.example.Ex3;
+
+public class Singer extends Artist {
+    public Singer(String name, int age, String skills) {
+        super(name, age, skills);
+    }
+}
