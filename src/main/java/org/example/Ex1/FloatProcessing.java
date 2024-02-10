4bb7a0b57eade8802b357bdecd927cd3d3e15208
Serhii Horb <43185034+Serhii-Horb@users.noreply.github.com>
2024-02-10 12:39:15 +0100

    Update FloatProcessing.java

--- src/main/java/org/example/Ex1/FloatProcessing.java
+++ src/main/java/org/example/Ex1/FloatProcessing.java
@@ -1,1 +1,27 @@
-package org.example.Ex1;public class FloatProcessing implements NumericalOperations<Float> {    @Override    public Float add(Float a, Float b) {        return a + b;    }    @Override    public Float sub(Float a, Float b) {        return a - b;    }    @Override    public Float multi(Float a, Float b) {        return a * b;    }    @Override    public Float div(Float a, Float b) {        if (b != 0) {            return a / b;        } else {            throw new ArithmeticException("На 0 делить нельзя");        }    }}
+package org.example.Ex1;
+
+public class FloatProcessing implements NumericalOperations<Float> {
+    @Override
+    public Float add(Float a, Float b) {
+        return a + b;
+    }
+
+    @Override
+    public Float sub(Float a, Float b) {
+        return a - b;
+    }
+
+    @Override
+    public Float multi(Float a, Float b) {
+        return a * b;
+    }
+
+    @Override
+    public Float div(Float a, Float b) {
+        if (b != 0) {
+            return a / b;
+        } else {
+            throw new ArithmeticException("На 0 делить нельзя");
+        }
+    }
+}
