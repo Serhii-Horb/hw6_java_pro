5be228828b713c8f22dd5016ecdd685ecfb0f805
Serhii Horb <43185034+Serhii-Horb@users.noreply.github.com>
2024-02-10 12:41:45 +0100

    Update Artist.java

--- src/main/java/org/example/Ex3/Artist.java
+++ src/main/java/org/example/Ex3/Artist.java
@@ -1,1 +1,42 @@
-package org.example.Ex3;public abstract class Artist {    private String name;    private int age;    private String skills;    public Artist(String name, int age, String skills) {        this.name = name;        this.age = age;        this.skills = skills;    }    public String getName() {        return name;    }    public void setName(String name) {        this.name = name;    }    public int getAge() {        return age;    }    public void setAge(int age) {        this.age = age;    }    public String getSkills() {        return skills;    }    public void setSkills(String skills) {        this.skills = skills;    }    public void showsTalent() {        System.out.println("Всем привет меня зовут " + name + " мне " + age + " я умею " + skills);    }}
+package org.example.Ex3;
+
+public abstract class Artist {
+    private String name;
+    private int age;
+    private String skills;
+
+    public Artist(String name, int age, String skills) {
+        this.name = name;
+        this.age = age;
+        this.skills = skills;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getSkills() {
+        return skills;
+    }
+
+    public void setSkills(String skills) {
+        this.skills = skills;
+    }
+
+    public void showsTalent() {
+        System.out.println("Всем привет меня зовут " + name + " мне " + age + " я умею " + skills);
+    }
+
+}
