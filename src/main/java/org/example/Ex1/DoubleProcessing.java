f19a7e1cc8e1d897087b2f193a63fcdd3a16ef65
Serhii Horb <43185034+Serhii-Horb@users.noreply.github.com>
2024-02-10 12:39:04 +0100

    Update DoubleProcessing.java

--- src/main/java/org/example/Ex1/DoubleProcessing.java
+++ src/main/java/org/example/Ex1/DoubleProcessing.java
@@ -1,1 +1,27 @@
-package org.example.Ex1;public class DoubleProcessing implements NumericalOperations<Double> {    @Override    public Double add(Double a, Double b) {        return a + b;    }    @Override    public Double sub(Double a, Double b) {        return a - b;    }    @Override    public Double multi(Double a, Double b) {        return a * b;    }    @Override    public Double div(Double a, Double b) {        if (b != 0) {            return a / b;        } else {            throw new ArithmeticException("На 0 делить нельзя");        }    }}
+package org.example.Ex1;
+
+public class DoubleProcessing implements NumericalOperations<Double> {
+    @Override
+    public Double add(Double a, Double b) {
+        return a + b;
+    }
+
+    @Override
+    public Double sub(Double a, Double b) {
+        return a - b;
+    }
+
+    @Override
+    public Double multi(Double a, Double b) {
+        return a * b;
+    }
+
+    @Override
+    public Double div(Double a, Double b) {
+        if (b != 0) {
+            return a / b;
+        } else {
+            throw new ArithmeticException("На 0 делить нельзя");
+        }
+    }
+}
